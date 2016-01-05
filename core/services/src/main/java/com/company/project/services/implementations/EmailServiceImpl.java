b51fe6679f83f7baa2afcd470b2625c403799cc0
Alan Albertengo <alan.albertengo@santexgroup.com>
2016-01-05 16:47:17 -0300

    fixed unit test

--- core/services/src/main/java/com/company/project/services/implementations/EmailServiceImpl.java
+++ core/services/src/main/java/com/company/project/services/implementations/EmailServiceImpl.java
@@ -4,21 +4,24 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.concurrent.Future;
 
-import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
 import com.company.project.services.interfaces.EmailService;
+import com.company.project.services.utils.InternationalizationService;
 
 import freemarker.template.Configuration;
 import freemarker.template.Template;
@@ -27,80 +30,109 @@ import freemarker.template.TemplateException;
 @Transactional
 @Service
 public class EmailServiceImpl implements EmailService {
-	private static final String CONFIRMATION_TEMPLATE = "confirmation.ftl";
-	private static final String FORGOT_PASSWORD_TEMPLATE = "forgotPassword.ftl";
+    private static final Logger log = Logger.getLogger(EmailServiceImpl.class);
 
-	private JavaMailSender mailSender;
-	private Configuration config;
+    private static final String CONFIRMATION_TEMPLATE = "confirmation.ftl";
+    private static final String FORGOT_PASSWORD_TEMPLATE = "forgotPassword.ftl";
 
-	@Value("${smtp.from}")
-	private String name;
+    private static final String CONFIRMATION_SUBJECT = "email.confirmation.subject";
+    private static final String NEW_PASSWORD_SUBJECT = "email.forgot.password.subject";
 
-	@Value("${smtp.username}")
-	private String email;
+    private final InternationalizationService i18nService;
+    private JavaMailSender mailSender;
+    private Configuration config;
 
-	@Autowired
-	public EmailServiceImpl(JavaMailSender mailSender, Configuration config) {
-		this.mailSender = mailSender;
-		this.config = config;
-	}
+    @Value("${smtp.from}")
+    private String name;
 
-	private Template getTemplate(String templateName, Locale locale) throws IOException, TemplateException {
-		return config.getTemplate(templateName, locale);
-	}
+    @Value("${smtp.username}")
+    private String email;
 
-	@Async
-	@Override
-	public void sendMail(String to, String subject, String body) {
-		SimpleMailMessage message = new SimpleMailMessage();
-		message.setTo(to);
-		message.setSubject(subject);
-		message.setText(body);
-		mailSender.send(message);
-	}
+    @Autowired
+    public EmailServiceImpl(JavaMailSender mailSender, Configuration config, InternationalizationService i18nService) {
+        this.mailSender = mailSender;
+        this.config = config;
+        this.i18nService = i18nService;
+    }
 
-	@Override
-	public void sendHTMLMessage(String to, String subject, Locale locale, String templateName, Map<String, String> templateVars) {
-		try {
-			Template template = getTemplate(templateName, locale);
-			String body = FreeMarkerTemplateUtils.processTemplateIntoString(template, templateVars);
+    private Template getTemplate(String templateName, Locale locale) throws IOException, TemplateException {
+        return config.getTemplate(templateName, locale);
+    }
 
-			MimeMessage message = mailSender.createMimeMessage();
-			message.setSubject(subject);
-			MimeMessageHelper helper = new MimeMessageHelper(message, true);
-			String from = String.format("%s <%s>", name, email);
-			helper.setFrom(from);
-			helper.setTo(to);
-			helper.setText(body, true);
-			mailSender.send(message);
+    @Async
+    @Override
+    public void sendMail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        mailSender.send(message);
+    }
 
-		} catch (IOException | TemplateException e) {
-			e.printStackTrace();
-		} catch (MessagingException e) {
-			e.printStackTrace();
-		}
-	}
+    @Override
+    public void sendHTMLMessage(String to, String subject, Locale locale, String templateName, Map<String, String> templateVars) throws Exception {
+        Template template = getTemplate(templateName, locale);
+        String body = FreeMarkerTemplateUtils.processTemplateIntoString(template, templateVars);
 
-	@Async
-	@Override
-	public void sendConfirmationMessage(String to, String subject, Locale locale, String name, String link) {
-		Map<String, String> templateVars = new HashMap<String, String>();
-		templateVars.put("name", name);
-		templateVars.put("confirmationLink", link);
+        MimeMessage message = mailSender.createMimeMessage();
+        message.setSubject(subject);
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        String from = String.format("%s <%s>", name, email);
+        helper.setFrom(from);
+        helper.setTo(to);
+        helper.setText(body, true);
+        mailSender.send(message);
+    }
 
-		sendHTMLMessage(to, subject, locale, CONFIRMATION_TEMPLATE, templateVars);
-	}
+    @Async
+    @Override
+    public Future<Boolean> sendAsyncMail(String to, String subject, String body) {
+        try {
+            sendMail(to, subject, body);
 
-	@Async
-	@Override
-	public void sendForgotPasswordMessage(String to, String subject, Locale locale, String name, String link) {
-		Map<String, String> templateVars = new HashMap<String, String>();
-		templateVars.put("name", name);
-		templateVars.put("forgotPasswordLink", link);
-		// TODO: create new link
-		templateVars.put("forgotPasswordNotRequestedLink", "create new link");
+            return new AsyncResult<Boolean>(true);
+        } catch (Exception e) {
+            log.error("The async email has not been send to: " + to, e);
+            return new AsyncResult<Boolean>(false);
+        }
+    }
 
-		sendHTMLMessage(to, subject, locale, FORGOT_PASSWORD_TEMPLATE, templateVars);
-	}
+    @Async
+    @Override
+    public Future<Boolean> sendConfirmationMessage(String to, String language, String name, String link) {
+        try {
+            Map<String, String> templateVars = new HashMap<String, String>();
+            templateVars.put("name", name);
+            templateVars.put("confirmationLink", link);
+
+            String subject = i18nService.getMessage(CONFIRMATION_SUBJECT, language);
+            sendHTMLMessage(to, subject, new Locale(language), CONFIRMATION_TEMPLATE, templateVars);
+
+            return new AsyncResult<Boolean>(true);
+        } catch (Exception e) {
+            log.error("The confirmation email has not been send to: " + to, e);
+            return new AsyncResult<Boolean>(false);
+        }
+    }
+
+    @Async
+    @Override
+    public Future<Boolean> sendForgotPasswordMessage(String to, String language, String name, String link) {
+        try {
+            Map<String, String> templateVars = new HashMap<String, String>();
+            templateVars.put("name", name);
+            templateVars.put("forgotPasswordLink", link);
+            // TODO: create new link
+            templateVars.put("forgotPasswordNotRequestedLink", "create new link");
+
+            String subject = i18nService.getMessage(NEW_PASSWORD_SUBJECT, language);
+            sendHTMLMessage(to, subject, new Locale(language), FORGOT_PASSWORD_TEMPLATE, templateVars);
+
+            return new AsyncResult<Boolean>(true);
+        } catch (Exception e) {
+            log.error("The confirmation email has not been send to: " + to, e);
+            return new AsyncResult<Boolean>(false);
+        }
+    }
 
 }
--- core/services/src/test/java/com/company/project/services/implementations/EmailServiceImplIntegrationTest.java
+++ core/services/src/test/java/com/company/project/services/implementations/EmailServiceImplIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.company.project.services.implementations;
 
-import java.util.Locale;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 import org.junit.Before;
 import org.junit.Ignore;
@@ -15,30 +16,66 @@ import com.company.project.services.interfaces.EmailService;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:testApplicationContext.xml")
 public class EmailServiceImplIntegrationTest {
+    private static final String SPANISH = "es";
+    private static final String PORTUGUES = "pt";
 
-	private static final Locale SPANISH = new Locale("es");
-	@Autowired
-	private EmailService emailService;
+    private @Autowired EmailService emailService;
 
-	@Before
-	public void setUp() {
-	}
+    @Before
+    public void setUp() {
+    }
 
-	@Ignore
-	@Test
-	public void testSendSimpleEmail() {
-		String to = "alan.albertengo@gmail.com";
-		String subject = "subject";
-		String body = "body";
-		emailService.sendMail(to, subject, body);
-	}
+    @Ignore
+    @Test
+    public void testSendSimpleEmail() throws InterruptedException, ExecutionException {
+        String to = "alan.albertengo@gmail.com";
+        String subject = "subject";
+        String body = "body";
+        Future<Boolean> future = emailService.sendAsyncMail(to, subject, body);
 
-	@Ignore
-	@Test
-	public void testSendHTMLEmail() {
-		String to = "alan.albertengo@gmail.com";
-		String subject = "subject";
-		emailService.sendConfirmationMessage(to, subject, SPANISH, "Alan", "linkValue");
-	}
+        while (true) {
+            if (future.isDone()) {
+                System.out.println("Simple email sent = " + future.get());
+                break;
+            }
+            System.out.println("Waiting to send Simple email. ");
+            Thread.sleep(1000);
+        }
+    }
+
+    @Ignore
+    @Test
+    public void testSendConfirmationEmail() throws InterruptedException, ExecutionException {
+        String to = "alan.albertengo@gmail.com";
+        String name = "Alan";
+
+        Future<Boolean> future = emailService.sendConfirmationMessage(to, SPANISH, name, "linkValue");
+
+        while (true) {
+            if (future.isDone()) {
+                System.out.println("Confirmation email sent = " + future.get());
+                break;
+            }
+            System.out.println("Waiting to send Confirmation email.");
+            Thread.sleep(1000);
+        }
+    }
+
+    @Ignore
+    @Test
+    public void testSendForgetPasswordEmail() throws InterruptedException, ExecutionException {
+        String name = "Alan";
+        String to = "alan.albertengo@gmail.com";
+        Future<Boolean> future = emailService.sendForgotPasswordMessage(to, PORTUGUES, name, "linkValue");
+
+        while (true) {
+            if (future.isDone()) {
+                System.out.println("Forgot Password email sent = " + future.get());
+                break;
+            }
+            System.out.println("Waiting to send Forgot Password email.");
+            Thread.sleep(1000);
+        }
+    }
 
 }
